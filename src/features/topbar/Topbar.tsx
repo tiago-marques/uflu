a2fce2904af70330bd00603674b1c4471329c9af
Tiago Marques <tmarques@dlocal.com>
2023-05-07 20:08:06 -0300

    mock open menu

--- src/features/topbar/Topbar.tsx
+++ src/features/topbar/Topbar.tsx
@@ -20,26 +20,31 @@ export function Topbar () {
                     </a>
                 </div>
                 <div className='flex lg:hidden'>
-                    <button
-                        type='button'
-                        className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+                    <Link
+                        to='/@surfistaperdido'
+                        className='text-sm font-semibold leading-6 text-gray-900'
                     >
-                        <span className='sr-only'>Open main menu</span>
-                        <svg
-                            className='h-6 w-6'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth='1.5'
-                            stroke='currentColor'
-                            aria-hidden='true'
+                        <button
+                            type='button'
+                            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
                         >
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-                            />
-                        </svg>
-                    </button>
+                            <span className='sr-only'>Open main menu</span>
+                            <svg
+                                className='h-6 w-6'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                strokeWidth='1.5'
+                                stroke='currentColor'
+                                aria-hidden='true'
+                            >
+                                <path
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+                                />
+                            </svg>
+                        </button>
+                    </Link>
                 </div>
                 <div className='hidden lg:flex lg:gap-x-12'></div>
                 <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
