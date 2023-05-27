893a3eaff26b11f3d380f21eac276efed64b2dd2
Tiago Marques <tmarques@dlocal.com>
2023-05-27 19:37:30 -0300

    build & flow

--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -98,6 +98,9 @@ const Login = () => {
             onSuccess={successResponseInstagram}
             onFailure={failResponseInstagram}
             redirectUri={'https://uflu.shop/'}
+            scope={
+                'user_profile,user_media,instagram_graph_user_profile,instagram_graph_user_media'
+            }
             cssClass='mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'
         >
             <img
