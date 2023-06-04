88bea786644b4bf03b7f6d989608c3d45365b75a
Tiago Marques <tmarques@dlocal.com>
2023-06-03 23:09:02 -0300

    update dlocal link

--- src/features/checkout/Checkout.tsx
+++ src/features/checkout/Checkout.tsx
@@ -10,7 +10,7 @@ export function Checkout () {
     return (
         <div className='flex-1 min-w-full md:min-w-0 overflow-auto'>
             <Iframe
-                url='https://checkout.dlocalgo.com/validate/uwxZMGDMBiYFJ46YnjtEGb2vmk2J6SMm'
+                url='https://checkout.dlocalgo.com/validate/Aq8wx5t7tnC9hwHcNZ3JbcgR7TO9gj4I'
                 id='checkout'
                 className='container mx-auto p-4 mb-6 bg-white rounded-xl min-h-screen'
                 display='block'
